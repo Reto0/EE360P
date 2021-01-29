146846513cb15f17faf1289b4b5a5b6d395d905a
Reto0 <45675750+Reto0@users.noreply.github.com>
2021-01-28 21:22:10 -0600

    PSort

--- HW1/PMerge.java
+++ HW1/PMerge.java
@@ -7,5 +7,6 @@ import java.util.concurrent.*;
 public class PMerge{
   public static void parallelMerge(int[] A, int[] B, int[]C, int numThreads){
     // TODO: Implement your parallel merge function
+    
   }
 }
--- HW1/PSort.java
+++ HW1/PSort.java
@@ -1,4 +1,4 @@
-//UT-EID=
+//UT-EID= rt25884   trm2796
 
 
 import java.util.*;
@@ -6,8 +6,10 @@ import java.util.concurrent.*;
 
 public class PSort{
     public static void parallelSort(int[] A, int begin, int end){
-    // TODO: Implement your parallel sort function 
-    
+        int processors = Runtime.getRuntime().availableProcessors();
+        QuickSort qs = new QuickSort(A, begin, end);
+        ForkJoinPool pool = new ForkJoinPool(processors);
+        pool.invoke(qs);
     }
 
     public static void insertionSort(int[] A, int begin, int end){
@@ -21,9 +23,45 @@ public class PSort{
     }
 
     public static class QuickSort extends RecursiveAction{
-        QuickSort(){}
-        protected void compute(){
 
+        int[] A;
+        int begin;
+        int end;
+        QuickSort(int[] A, int begin, int end){
+            this.A = A;
+            this.begin = begin;
+            this.end = end;
+        }
+
+        @Override
+        protected void compute(){
+            if(end-begin < 16)
+                insertionSort(A, begin, end);
+            else{
+                int pivot = partition(A, begin, end);
+                QuickSort q1 = new QuickSort(A, begin, pivot);
+                q1.fork();
+                QuickSort q2 = new QuickSort(A, pivot, end);
+                q2.compute();
+                q1.join();
+            }
+        }
+
+        protected static int partition(int[] A, int begin, int end){
+            int pivot = A[end-1];
+            int i = begin-1;
+            for(int j = begin; j <= end; j++){
+                if(A[j] < pivot){
+                    i++;
+                    int temp = A[i];
+                    A[i] = A[j];
+                    A[j] = temp;
+                }
+            }
+            int temp = A[i+1];
+            A[i+1] = A[end-1];
+            A[end-1] = temp;
+            return i+1;
         }
     }
 }
