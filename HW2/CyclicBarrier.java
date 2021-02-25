81608e3ea641b47177cf4e96df5cb94e26c4cd8f
Reto0 <45675750+Reto0@users.noreply.github.com>
2021-02-25 13:38:10 -0600

    fixed cyclicbarrier bug

--- HW2/CyclicBarrier.java
+++ HW2/CyclicBarrier.java
@@ -3,12 +3,13 @@
  * trm2796
  */
 import java.util.concurrent.Semaphore; // for implementation using Semaphores
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CyclicBarrier {
 	private Semaphore barrierLock = new Semaphore(0);
     private Semaphore mutex = new Semaphore(1);
     private Semaphore limit;
-    private int count = 0;
+    private AtomicInteger count = new AtomicInteger(0);
     private int parties;
 	public CyclicBarrier(int parties) {
         this.limit = new Semaphore(parties);
@@ -17,20 +18,23 @@ public class CyclicBarrier {
 	
 	public int await() throws InterruptedException {
         mutex.acquire();
-        limit.acquire();
+        // limit.acquire();
         
-        count++;
-        int index = parties-count;
-        if(count != parties){
+        count.incrementAndGet();
+        int index = parties-count.get();
+        if(count.get() != parties){
             mutex.release();
             barrierLock.acquire();
         }
         else{
             barrierLock.release(parties-1);
-            count = 0;
-            limit.release(parties);
-            mutex.release();
+            // count = 0;
+            // limit.release(parties);
+            // mutex.release();
         }
+        count.decrementAndGet();
+        if(count.get() == 0)
+            mutex.release();
 	    return index;
 	}
 }
